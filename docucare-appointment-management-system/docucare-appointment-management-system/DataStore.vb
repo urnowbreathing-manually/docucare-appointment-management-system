31938ca9f57b3b3becbcb3289599201d9f0abe23
urnowbreathing-manually <andreizantua.ucc1@gmail.com>
2025-10-20 16:39:56 +0100

    Added DataStore.vb, Added gitignore

---
+++ docucare-appointment-management-system/docucare-appointment-management-system/DataStore.vb
@@ -0,0 +1,12 @@
+﻿Module DataStore
+    'Hardcoded list of all existing personel in the hospital.
+    Public personnelDB(,) As String = {
+                                        {"wionquintela", "adminPass", "0001-A", "Admin"},
+                                        {"jestineaquino", "abc123", "0001-D", "Doctor"},
+                                        {"andreizantua", "def456", "0002-D", "Doctor"},
+                                        {"kaelrivera", "staffPass", "0001-S", "Staff"}
+                                    }
+
+    'Stores the ID of the current user for reference
+    Public currentUser() As String = {"", "", "", ""}
+End Module
