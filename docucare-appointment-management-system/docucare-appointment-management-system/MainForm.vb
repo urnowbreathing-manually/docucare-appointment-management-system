aef198bfe14991839208f32aa66739f59fb596de
urnowbreathing-manually <andreizantua.ucc1@gmail.com>
2025-10-21 14:27:11 +0100

    Added MySQL connection, added DB-based user verification

--- docucare-appointment-management-system/docucare-appointment-management-system/MainForm.vb
+++ docucare-appointment-management-system/docucare-appointment-management-system/MainForm.vb
@@ -1,6 +1,24 @@
-﻿Public Class MainForm
+﻿
+Public Class MainForm
+
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.StartPosition = FormStartPosition.CenterScreen
+
+        'ConnectionString = "server=localhost;user=root;database=DocUCare;port=3306;password=washer22456;"
+
+
+        'TEST DB CONNECTION
+
+        'MsgBox("Hello World!")
+        'Try
+        '    conn.Open()
+        '    MsgBox("Connected!")
+        'Catch ex As Exception
+        '    MsgBox(ex.Message)
+
+        'End Try
+        'conn.Close()
+
     End Sub
 
     Private Sub MainContentPanel_Paint(sender As Object, e As PaintEventArgs) Handles MainContentPanel.Paint
--- docucare-appointment-management-system/docucare-appointment-management-system/UcAuthForm.vb
+++ docucare-appointment-management-system/docucare-appointment-management-system/UcAuthForm.vb
@@ -1,10 +1,21 @@
-﻿Public Class UcAuthForm
+﻿Imports System.Net.Security
+Imports MySql.Data.MySqlClient
+Public Class UcAuthForm
     Private MainContentPanel As Panel
     ' Constructor receives parent panel
+
+    Dim ConnectionString As String = "server=localhost;user=root;database=DocUCare;port=3306;password=washer22456;"
+    Dim conn As New MySqlConnection(ConnectionString)
+    Dim cmd As MySqlCommand
+    Dim da As New MySqlDataAdapter
+    Dim dReader As MySqlDataReader
+
+
     Public Sub New(parent As Panel)
         InitializeComponent()
         MainContentPanel = parent
     End Sub
+
 
     Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
         DataStore.currentUser = {"", "", "", ""}
@@ -27,42 +38,69 @@
 
         'Account Verification
 
-        For i As Integer = 0 To 3
-            If UserName.Text = DataStore.personnelDB(i, 0) Then
-                If Password.Text <> DataStore.personnelDB(i, 1) Then
-                    i = 0
-                    MessageBox.Show("Incorrect Password.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-                    Password.Focus()
-                    Exit Sub
-                End If
+        'For i As Integer = 0 To 3
+        '    If UserName.Text = DataStore.personnelDB(i, 0) Then
+        '        If Password.Text <> DataStore.personnelDB(i, 1) Then
+        '            i = 0
+        '            MessageBox.Show("Incorrect Password.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        '            Password.Focus()
+        '            Exit Sub
+        '        End If
 
-                If VerifiedID.Text <> DataStore.personnelDB(i, 2) Then
-                    i = 0
-                    MessageBox.Show("Incorrect ID.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-                    VerifiedID.Focus()
-                    Exit Sub
-                End If
+        '        If VerifiedID.Text <> DataStore.personnelDB(i, 2) Then
+        '            i = 0
+        '            MessageBox.Show("Incorrect ID.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        '            VerifiedID.Focus()
+        '            Exit Sub
+        '        End If
 
-                DataStore.currentUser = {UserName.Text, Password.Text, VerifiedID.Text, DataStore.personnelDB(i, 3)}
+        '        DataStore.currentUser = {UserName.Text, Password.Text, VerifiedID.Text, DataStore.personnelDB(i, 3)}
+        '        MainContentPanel.Controls.Clear()
+        '        Dim addMainMenu As New UcMainMenu(MainContentPanel)
+        '        addMainMenu.Dock = DockStyle.Fill
+        '        MainContentPanel.Controls.Add(addMainMenu)
+        '        Exit For
+
+        '    ElseIf (UserName.Text <> DataStore.personnelDB(i, 0)) And (i = 3) Then
+        '        MessageBox.Show("User does not exist.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        '        UserName.Focus()
+        '        Exit Sub
+        '    End If
+        'Next
+
+        Dim sql As String = "SELECT Personnel_Name, Password, Verified_ID FROM PersonnelTable WHERE Personnel_Name ='" & UserName.Text & "' and Password = '" & Password.Text & "' and Verified_ID = '" & VerifiedID.Text & "'"
+
+        Try
+            conn.Open()
+            cmd = New MySqlCommand(sql, conn)
+            dReader = cmd.ExecuteReader()
+
+            If dReader.Read() Then
+                MsgBox(dReader("Personnel_Name").ToString)
                 MainContentPanel.Controls.Clear()
                 Dim addMainMenu As New UcMainMenu(MainContentPanel)
                 addMainMenu.Dock = DockStyle.Fill
                 MainContentPanel.Controls.Add(addMainMenu)
-                Exit For
-
-            ElseIf (UserName.Text <> DataStore.personnelDB(i, 0)) And (i = 3) Then
-                MessageBox.Show("User does not exist.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Else
+                MessageBox.Show("Incorrect Name/Password/Verified ID", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
                 UserName.Focus()
-                Exit Sub
             End If
-        Next
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation)
+        Finally
+            conn.Close()
+            da.Dispose()
+        End Try
+
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles UserName.TextChanged
         Dim tb As TextBox = DirectCast(sender, TextBox)
 
         ' Remove anything that's not a letter or space
-        tb.Text = System.Text.RegularExpressions.Regex.Replace(tb.Text, "[^a-zA-Z\s]", "")
+        'tb.Text = System.Text.RegularExpressions.Regex.Replace(tb.Text, "[^a-zA-Z\s]", "")
 
         ' Move cursor to the end (otherwise it jumps back)
         tb.SelectionStart = tb.Text.Length
